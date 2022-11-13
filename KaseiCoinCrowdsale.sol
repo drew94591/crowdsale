220b3f2431a84ced7d7bfdf3fad4941e8f784ef4
Andrew Herrera <drew94591@gmail.com>
2022-11-13 08:11:46 -0800

    Added implementation of CappedCrowdsale, TimedCrowdsale, and RefundablePostDeliveryCrowdsale

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -3,13 +3,31 @@ pragma solidity ^0.5.5;
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
-contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
+contract KaseiCoinCrowdsale is
+    Crowdsale,
+    MintedCrowdsale,
+    CappedCrowdsale,
+    TimedCrowdsale,
+    RefundablePostDeliveryCrowdsale
+{
     constructor(
         uint256 rate,
         address payable wallet,
-        KaseiCoin token
-    ) public Crowdsale(rate, wallet, token) {
+        KaseiCoin token,
+        uint256 goal,
+        uint256 open,
+        uint256 close
+    )
+        public
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(goal)
+        TimedCrowdsale(open, close)
+        RefundableCrowdsale(goal)
+    {
         // constructor can stay empty
     }
 }
@@ -21,14 +39,18 @@ contract KaseiCoinCrowdsaleDeployer {
     constructor(
         string memory name,
         string memory symbol,
-        address payable wallet
+        address payable wallet,
+        uint256 goal
     ) public {
         KaseiCoin token = new KaseiCoin(name, symbol, 0);
         kasei_token_address = address(token);
         KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale(
             1,
             wallet,
-            token
+            token,
+            goal,
+            now,
+            now + 24 weeks
         );
         kasei_crowdsale_address = address(kasei_crowdsale);
         token.addMinter(kasei_crowdsale_address);
